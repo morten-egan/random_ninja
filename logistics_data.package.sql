f11486e5334811e4812415e8570938146a9c12f7
Morten Egan <morten@egan.dk>
2021-04-12 17:01:34 +0800

    Adding logistics functions

--- logistics_data.package.sql
+++ logistics_data.package.sql
@@ -13,6 +13,13 @@ as
     type country_bics is table of bic_data_tab index by varchar2(200);
     bics            country_bics;
 
+    type iso_container_rec is record (
+        iso_code            varchar2(50)
+        , iso_description   varchar2(500)
+    );
+    type iso_container_size_tab is table of iso_container_rec;
+    iso_container_sizes     iso_container_size_tab;
+
     init_data   varchar2(250);
 
 end logistics_data;
--- logistics_random.package.sql
+++ logistics_random.package.sql
@@ -5,8 +5,25 @@ as
     npg_version         varchar2(250) := '1.3.0';
 
     function r_container_bic (
-        r_country           varchar2        default null
+        r_country                   varchar2        default null
     )
+    return varchar2;
+
+    function r_container_bic_company (
+        r_country                   varchar2        default null
+    )
+    return varchar2;
+
+    function r_container_serial_number
+    return number;
+
+    function r_container_number (
+        r_container_bic             varchar2        default null
+        , r_container_serial_number number          default null
+    )
+    return varchar2;
+
+    function r_container_size_iso
     return varchar2;
 
 end logistics_random;
